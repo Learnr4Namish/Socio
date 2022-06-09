b6b58720ea3af55a22041976d826da88a39be610
Learnr4Namish <97686147+Learnr4Namish@users.noreply.github.com>
2022-06-09 12:02:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -115,6 +115,41 @@ document.getElementById("SocioPassText").value = "";
 <option value="november">November</option>
 <option value="december">December</option>
 </select>
+       <p style={normalText}>Choose your DOB day:</p>
+<select name="days" id="dobDays" className="selectPicker">
+  <option value={1}>1</option>
+  <option value={2}>2</option>
+  <option value={3}>3</option>
+  <option value={4}>4</option>
+  <option value={5}>5</option>
+  <option value={6}>6</option>
+  <option value={7}>7</option>
+  <option value={8}>8</option>
+  <option value={9}>9</option>
+  <option value={10}>10</option>
+  <option value={11}>11</option>
+  <option value={12}>12</option>
+  <option value={13}>13</option>
+  <option value={14}>14</option>
+  <option value={15}>15</option>
+  <option value={16}>16</option>
+  <option value={17}>17</option>
+  <option value={18}>18</option>
+  <option value={19}>19</option>
+  <option value={20}>20</option>
+  <option value={21}>21</option>
+  <option value={22}>22</option>
+  <option value={23}>23</option>
+  <option value={24}>24</option>
+  <option value={25}>25</option>
+  <option value={26}>26</option>
+  <option value={27}>27</option>
+  <option value={28}>28</option>
+  <div id="leapyear"><option value={29}>29</option></div>
+  <div id="februaryRe"<option value={30}>30</option></div>
+  <div id="monthEnder"><option value={31}>31</option></div>
+</select>
+
        <Button style={loginNameBtn} variant='contained' className='fullWidth upMarginLgn' onClick={nextDob}>Next</Button>
        </div>
       </React.StrictMode>
