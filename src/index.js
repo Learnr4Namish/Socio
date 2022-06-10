75600a679ab5cda2d877794920eba021b5514abb
Learnr4Namish <97686147+Learnr4Namish@users.noreply.github.com>
2022-06-10 15:39:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -100,74 +100,11 @@ document.getElementById("SocioPassText").value = "";
        <div className='container'>
        <Typography style={brandText} id="brandMiddleText">Socio</Typography>
        <p style={normalText}>When were you born? Please notice that you must be of atleast <b>18 years</b> of age.</p>
-       <p style={normalText}>Choose your DOB month:</p>
-<select name="months" id="dobMonths" onChange={changeDobDays} className="selectPicker">
-  <option value="january">January</option>
-  <option value="february">February</option>
-  <option value="march">March</option>
-  <option value="april">April</option>
-  <option value="may">May</option>
-  <option value="june">June</option>
-  <option value="july">July</option>
-  <option value="august">August</option>
-  <option value="september">September</option>
-<option value="october">October</option>
-<option value="november">November</option>
-<option value="december">December</option>
-</select>
-       <p style={normalText}>Choose your DOB day:</p>
-<select name="days" id="dobDays" className="selectPicker">
-  <option value={1}>1</option>
-  <option value={2}>2</option>
-  <option value={3}>3</option>
-  <option value={4}>4</option>
-  <option value={5}>5</option>
-  <option value={6}>6</option>
-  <option value={7}>7</option>
-  <option value={8}>8</option>
-  <option value={9}>9</option>
-  <option value={10}>10</option>
-  <option value={11}>11</option>
-  <option value={12}>12</option>
-  <option value={13}>13</option>
-  <option value={14}>14</option>
-  <option value={15}>15</option>
-  <option value={16}>16</option>
-  <option value={17}>17</option>
-  <option value={18}>18</option>
-  <option value={19}>19</option>
-  <option value={20}>20</option>
-  <option value={21}>21</option>
-  <option value={22}>22</option>
-  <option value={23}>23</option>
-  <option value={24}>24</option>
-  <option value={25}>25</option>
-  <option value={26}>26</option>
-  <option value={27}>27</option>
-  <option value={28}>28</option>
-  <option value={29}>29</option>
-  <option value={30}>30</option>
-  <option value={31}>31</option>
-</select>
-<p style={normalText}>Choose your DOB year:</p>
-<select name="years" id="dobYears" className="selectPicker"></select>
+      
        <button style={loginNameBtn} variant='contained' className='fullWidth upMarginLgn' onClick={nextDob}>Next</button>
        </div>
       </React.StrictMode>
         );
-const renderDobYears = document.getElementById("dobYears");
-const thisYearGet = new Date().getFullYear();
-const hundredYearsBack= Number(thisYearGet) - 100;
-console.log(hundredYearsBack);
-for (let i = Number(hundredYearsBack); i <= Number(thisYearGet); i++) {
- renderDobYears.innerHTML += "<option value='" + i + "'>" + i + "</option>";
 }
-}
-}
-function changeDobDays() {
-  const targetSelectorMonth = document.getElementById("dobMonths");
-  const targetSelectorDay = document.getElementById("dobDays");
- if(targetSelectorMonth.selectedIndex === 2) {
-  targetSelectorDay.remove(30);
 }
 }
